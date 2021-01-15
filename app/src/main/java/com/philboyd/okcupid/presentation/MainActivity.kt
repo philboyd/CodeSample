b2bb876ff1b16c1354fe5408d8e2a2ca93ed964e
philboyd <philboyd@stockx.com>
2021-01-14 20:48:02 -0500

    Move activity to presentation layer

--- app/src/main/java/com/philboyd/okcupid/presentation/MainActivity.kt
+++ app/src/main/java/com/philboyd/okcupid/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.philboyd.okcupid
+package com.philboyd.okcupid.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.philboyd.okcupid.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
